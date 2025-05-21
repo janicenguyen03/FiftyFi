39650c29a29cfc46ed18ec304bd51a82a8bd72df
Janice Nguyen <haidn@uci.edu>
2025-05-21 01:19:59 -0700

    edit samesite cookie

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,7 @@ let sessionOptions = {
   cookie: { 
     maxAge: 24 * 60 * 60 * 1000,
     secure: process.env.NODE_ENV === "production", 
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     httpOnly: true }
 };
 
